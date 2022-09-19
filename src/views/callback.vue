278bd3345788fa66336fb72d8c9f2951214f23c2
Chambers <bobhus394@gmail.com>
2022-09-19 15:16:38 +0800

    開啟測試模式

--- src/views/callback.vue
+++ src/views/callback.vue
@@ -1,36 +1,11 @@
-<script setup>
-import { ref } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-import { useLineStore } from '../stores/line'
-import api from '../utils/api'
-const store = useLineStore()
-
-const route = useRoute()
-const code = route.query.code
-const state = route.query.state
-const err = ref(false)
-
-const router = useRouter()
-api.auth(code)
-  .then(res => {
-    store.access_token = res.access_token
-    router.replace('/')
-  })
-  .catch(() => {
-    err.value = true
-  })
-
-</script>
-
 <template>
   <div class=" text-2xl p-4">
-    <section v-if="err">
-      <p>讀取中</p>
-      <router-link to="/login">回登入頁面</router-link>
-    </section>
-    <section v-else>
-      <p>讀取中</p>
-      <p><i class='bx bx-loader bx-spin' ></i></p>
-    </section>
+    <div>code: {{code}}</div>
   </div>
 </template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const code = route.qurey.code
+</script>
